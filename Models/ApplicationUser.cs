8221664959565d83fc55e45bb251ee82cf07c861
Nhứt Nguyên <kieuphuongcsgo@gmail.com>
2025-02-21 10:33:56 +0700

    Add more models

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Models
 {
@@ -12,18 +7,13 @@ namespace Models
 
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public string Gender { get; set; }
-        public string PhoneNumber { get; set; } 
-
-        public string FullName { get; set; }
-
-
-        public string Address { get; set; } 
+        public string? Gender { get; set; } // Made nullable
+        public new string? PhoneNumber { get; set; } // Used new keyword and made nullable
+        public string? FullName { get; set; } // Made nullable
+        public string? Address { get; set; } // Made nullable
         public string? RefreshToken { get; set; }
         public DateTime? RefreshTokenExpiryTime { get; set; }
         public DateTime CreateAt { get; set; }
         public DateTime UpdateAt { get; set; }
-
-       
     }
 }
---
+++ Models/Order.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    [Table("Order")]
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+
+        [Required]
+        public int AccountId { get; set; }
+
+        [Required]
+        public string OrderStatus { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public decimal PriceTotal { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public bool PaymentConfirmed { get; set; }
+
+        [Required]
+        public string DeliveryAddress { get; set; }
+
+        [Required]
+        public string Note { get; set; }
+
+        [Required]
+        public int PhoneNumber { get; set; }
+
+        [Required]
+        public decimal DiscountMoney { get; set; }
+
+        public virtual ApplicationUser? Account { get; set; }
+
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+
+        public virtual ICollection<Voucher> Vouchers { get; set; }
+
+        public virtual ICollection<WalletTransaction> WalletTransactions { get; set; }
+    }
+}
